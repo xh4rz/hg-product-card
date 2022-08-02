3fba7705816f8f71b75eda9f800befee27fe34e4
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-08-01 21:30:52 -0500

    116. Pruebas en ProductTitle

---
+++ test/data/products.ts
@@ -0,0 +1,10 @@
+export const product1 = {
+  id: '1',
+  title: 'Coffee Mug - Sin Imagen',
+};
+
+export const product2 = {
+  id: '2',
+  title: 'Coffee Mug - Con Imagen',
+  img: './coffee-mug.png',
+};
